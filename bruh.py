a5e3438c5c1569ff68ae0a528d9dd25ad34bbe62
Romashka <80623570+RomashkaTea@users.noreply.github.com>
2022-12-14 21:50:55 +0300

    Create bruh.py

---
+++ bruh.py
@@ -0,0 +1,16 @@
+
+
+erase_array = ["userdata", "system", "vendor", "boot", "cache", "recovery", "misc", "abl", "modem", "modemst1", "modemst2", "fsg", "frp", "abl", "xblbak", "ablbak", "crclist", "sparsecrclist", "aboot", "bootloader", "xbl", "arb", "preloader", "aboot", "tz", "tzbak", "hyp", "hypbak", "rpm", "rpmbak", "devcfg", "devcfgbak", "cmnlib", "cmnlibbak", "cmnlib64", "cmnlib64bak", "dsp", "storsec", "storsecbak", "bluetooth", "keymaster", "keymasterbak", "logo", "splash"]
+erase_ab_array = ["system", "vendor", "boot", "recovery", "product"]
+lock_bootloader = True
+
+for i in erase_array:
+    print(f"fastboot erase {i}")
+
+for i in erase_ab_array:
+    print(f"fastboot erase {i}_a")
+    print(f"fastboot erase {i}_b")
+
+if lock_bootloader:
+    print("fastboot oem lock")
+    print("fastboot flashing lock")
